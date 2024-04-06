341646f4190dfcd483d02c90c2ab9babea18106f
Mathew Gabriel <99707207+MG-Minsu@users.noreply.github.com>
2024-04-06 21:25:19 +0800

    Update 1Supervised Learning.py

--- pages/1Supervised Learning.py
+++ pages/1Supervised Learning.py
@@ -98,8 +98,8 @@ def app():
 
 
         # Add labels and title using ax methods
-        ax.set_xlabel('bmi')
-        ax.set_ylabel('bp')
+        ax.set_xlabel('BMI')
+        ax.set_ylabel('BP')
         ax.set_title('Visualization of Diabetes Dataset')
 
 
